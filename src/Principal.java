f454453cce807076753dfbc70458eed9a019af57
GustavoPaluski <guorkut@gmail.com>
2023-11-27 17:13:15 -0300

    Primeiro

---
+++ src/Principal.java
@@ -0,0 +1,21 @@
+public class Principal {
+    public static void main(String[] args) {
+        
+        // [1] - Cadastro/login
+        //     modo admin
+        // [2] - Doacoes
+        //     criar Doacoes
+        //     visualizar doacoes 
+        // [3] - Trocas 
+        //     criar trocas
+        //     visualizar trocas
+        // [4] - Noticias
+        //     mostrar trocas 
+        // [5] - Grupos
+        //     criar Grupos
+        //     participar de grupos  
+        // [6] - perfil 
+        //     visualizar dados cadastrais 
+        // [7] - sair 
+    }
+}
